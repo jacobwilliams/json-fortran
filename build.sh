f4dc356858f5ab406e2ecd533f411e76cec3a680
jacobwilliams <jacobwilliams@users.noreply.github.com>
2014-06-23 02:15:02 -0500

    some changes to build.sh

--- build.sh
+++ build.sh
@@ -1,62 +1,77 @@
 #!/bin/bash
 
 #
-#  This is just a simple script to build the json-fortran library and example program on Linux and Mac.
+#  This is just a simple script to build the json-fortran library 
+#    and example program on Linux and Mac.
 #
 #  It also builds the documentation using RoboDoc
 #
 #  Jacob Williams : 2/8/2014
+#     - modified 6/23/2014
 #
 
 # Uncomment to debug
 #set -x
 
-SRCDIR='src/'
-BUILDDIR='lib/'
-BINDIR='bin/'
-DOCDIR='documentation/'
+# Set to 1 to use ifort, otherwise use gfortran
+use_ifort=0
 
-# Intel compiler
-FCOMPILER='ifort'
-# The following warning might be triggered by ifort unless explicitly silenced:
-# warning #7601: F2008 standard does not allow an internal procedure to be an actual argument procedure name. (R1214.4).
-# In the context of F2008 this is an erroneous warning.
-# See https://prd1idz.cps.intel.com/en-us/forums/topic/486629
-FCOMPILERFLAGS='-O2 -warn -stand f08 -diag-disable 7601 -traceback'
-#FCOMPILERFLAGS='-warn -traceback -stand f08 -assume protect_parens -assume buffered_io -check all'
-# trailing space is significant
-FCMODULEPATHFLAG='-module '
+SRCDIR='src/'                #source directory
+BUILDDIR='lib/'              #build directory for library
+BINDIR='bin/'                #build directory for executable
+DOCDIR='documentation/'      #build directory for documentation
+ARCHIVER='ar'                #archiver name
+ARCHIVERFLAGS='-cq'          #archiver flags
+FEXT='.f90'                  #fortran file extension
+OBJEXT='.o'                  #object code extension
+LIBEXT='.a'                  #static library extension
+MODEXT='.mod'                #fortran module file extension
+WC='*'                       #wildcard character
+LIBOUT='libjsonfortran'      #name of json library
+EXEOUT='json'                #name of example program
+MODCODE='json_module'        #json module file name (no extension)
+EXAMPLECODE='json_example'   #example program file name (no extension)
+ROBODOC='robodoc'            #robodoc executable name
+ROBOFLAGS="--src ${SRCDIR} --doc ${DOCDIR} --multidoc --html --ignore_case_when_linking --syntaxcolors --source_line_numbers --index --tabsize 4 --documenttitle jsonfortran --sections"  #robodoc flags
 
-# GFortran (must be >= 4.9)
-#FCOMPILER='gfortran'
-#FCOMPILER='/opt/local/bin/gfortran-mp-4.9'
-#FCOMPILERFLAGS='-O2 -fbacktrace -Wall -Wextra -Wno-maybe-uninitialized -pedantic -std=f2008'
-#FCMODULEPATHFLAG='-J'
+#
+# Compiler-specifics:
+#
 
-ARCHIVER='ar'
-ARCHIVERFLAGS='-cq'
+if [ $use_ifort -eq 1 ]
+then
 
-FEXT='.f90'
-OBJEXT='.o'
-LIBEXT='.a'
-MODEXT='.mod'
-WC='*'
+	# Intel compiler
+	
+	FCOMPILER='ifort'
+	# The following warning might be triggered by ifort unless explicitly silenced:
+	# warning #7601: F2008 standard does not allow an internal procedure to be an actual argument procedure name. (R1214.4).
+	# In the context of F2008 this is an erroneous warning.
+	# See https://prd1idz.cps.intel.com/en-us/forums/topic/486629
+	FCOMPILERFLAGS='-O2 -warn -stand f08 -diag-disable 7601 -traceback'
+	#FCOMPILERFLAGS='-warn -traceback -stand f08 -assume protect_parens -assume buffered_io -check all'
+	# trailing space is significant
+	FCMODULEPATHFLAG='-module '
 
-LIBOUT='libjsonfortran'
-EXEOUT='json'
+else
 
-MODCODE='json_module'
-EXAMPLECODE='json_example'
+	# GFortran (must be >= 4.9)
+	
+	FCOMPILER='gfortran'
+	#FCOMPILER='/opt/local/bin/gfortran-mp-4.9'
+	FCOMPILERFLAGS='-O2 -fbacktrace -Wall -Wextra -Wno-maybe-uninitialized -pedantic -std=f2008'
+	FCMODULEPATHFLAG='-J'
 
-ROBODOC='robodoc'
-ROBOFLAGS="--src ${SRCDIR} --doc ${DOCDIR} --multidoc --html --ignore_case_when_linking --syntaxcolors --source_line_numbers --index --tabsize 4 --documenttitle jsonfortran --sections"
+fi
 
-#output directories:
+#
+# Always a clean build:
+#
+
 mkdir -p $BUILDDIR
 mkdir -p $BINDIR
 mkdir -p $DOCDIR
 
-#clean build:
 rm -f $BUILDDIR$WC$OBJEXT
 rm -f $BUILDDIR$WC$MODEXT
 rm -f $BUILDDIR$WC$LIBEXT
