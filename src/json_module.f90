b2faf39ebb90129dbbb78d26bb345430411af73f
jacobwilliams <jacobwilliams@users.noreply.github.com>
2015-03-03 11:12:09 -0600

    minor formatting change for documentation header.

--- src/json_module.f90
+++ src/json_module.f90
@@ -19,57 +19,55 @@
 !
 !    json-fortran License:
 !
-!      JSON-FORTRAN: A Fortran 2008 JSON API
-!      http://github.com/jacobwilliams/json-fortran
+!    JSON-FORTRAN: A Fortran 2008 JSON API
+!
+!    http://github.com/jacobwilliams/json-fortran
 !  
-!      Copyright (c) 2014, Jacob Williams
-!      All rights reserved.
+!    Copyright (c) 2014, Jacob Williams
+!
+!    All rights reserved.
 !  
-!      Redistribution and use in source and binary forms, with or without modification,
-!      are permitted provided that the following conditions are met:
-!  
-!      * Redistributions of source code must retain the above copyright notice, this
-!        list of conditions and the following disclaimer.
-!  
-!      * Redistributions in binary form must reproduce the above copyright notice, this
-!        list of conditions and the following disclaimer in the documentation and/or
-!        other materials provided with the distribution.
-!  
-!      * The names of its contributors may not be used to endorse or promote products
-!        derived from this software without specific prior written permission.
-!  
-!      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-!      ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-!      WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-!      DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-!      ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-!      (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-!      LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-!      ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-!      (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-!      SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!    Redistribution and use in source and binary forms, with or without modification,
+!    are permitted provided that the following conditions are met:
+!    * Redistributions of source code must retain the above copyright notice, this
+!      list of conditions and the following disclaimer.  
+!    * Redistributions in binary form must reproduce the above copyright notice, this
+!      list of conditions and the following disclaimer in the documentation and/or
+!      other materials provided with the distribution.
+!    * The names of its contributors may not be used to endorse or promote products
+!      derived from this software without specific prior written permission.
+!    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+!    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+!    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+!    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+!    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+!    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+!    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+!    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+!    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+!    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 !
 !    Original FSON License:
 !
-!      http://github.com/josephalevin/fson [FSON code retrieved on 12/2/2013]
+!    http://github.com/josephalevin/fson
 !  
-!      Copyright (c) 2012 Joseph A. Levin
+!    Copyright (c) 2012 Joseph A. Levin
 !  
-!      Permission is hereby granted, free of charge, to any person obtaining a copy of this
-!      software and associated documentation files (the "Software"), to deal in the Software
-!      without restriction, including without limitation the rights to use, copy, modify, merge,
-!      publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-!      persons to whom the Software is furnished to do so, subject to the following conditions:
+!    Permission is hereby granted, free of charge, to any person obtaining a copy of this
+!    software and associated documentation files (the "Software"), to deal in the Software
+!    without restriction, including without limitation the rights to use, copy, modify, merge,
+!    publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+!    persons to whom the Software is furnished to do so, subject to the following conditions:
 !  
-!      The above copyright notice and this permission notice shall be included in all copies or
-!      substantial portions of the Software.
+!    The above copyright notice and this permission notice shall be included in all copies or
+!    substantial portions of the Software.
 !  
-!      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-!      INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-!      PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-!      LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
-!      OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-!      DEALINGS IN THE SOFTWARE.
+!    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+!    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+!    PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+!    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+!    OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+!    DEALINGS IN THE SOFTWARE.
 !
 !  HISTORY
 !    * Joseph A. Levin : March 2012 : Original FSON code [retrieved on 12/2/2013].
