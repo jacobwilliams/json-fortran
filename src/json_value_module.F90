c9a13e2d3254359e3ce538510e081a227124312f
Jacob Williams <jacobwilliams@users.noreply.github.com>
2016-04-28 00:03:20 -0500

    added initial version of swap routine.
    Also added some additional error checks for malformed JSON linked lists (prevents an infinite loop under some circumstances if the structure is not properly constructed).

--- src/json_value_module.F90
+++ src/json_value_module.F90
@@ -492,6 +492,12 @@
         procedure :: MAYBEWRAP(json_parse_string)
 
         !>
+        !  Swap two [[json_value]] pointers in a structure
+        !  (or two different structures).
+        generic,public :: swap => json_value_swap
+        procedure :: json_value_swap
+
+        !>
         !  Throw an exception.
         generic,public :: throw_exception => MAYBEWRAP(json_throw_exception)
         procedure :: MAYBEWRAP(json_throw_exception)
@@ -1148,9 +1154,15 @@
         if (associated(me%children)) then
             do while (me%n_children > 0)
                 p => me%children
-                me%children => me%children%next
-                me%n_children = me%n_children - 1
-                call json_value_destroy(json,p,.false.)
+                if (associated(p)) then
+                    me%children => me%children%next
+                    me%n_children = me%n_children - 1
+                    call json_value_destroy(json,p,.false.)
+                else
+                    call json%throw_exception('Error in json_value_destroy: '&
+                                              'Malformed JSON linked list')
+                    exit
+                end if
             end do
             nullify(me%children)
             nullify(p)
@@ -1203,7 +1215,6 @@
 !
 !# History
 !  * Jacob Williams : 12/28/2014 : added destroy optional argument.
-!
 
     subroutine json_value_remove(json,me,destroy)
 
@@ -1273,6 +1284,201 @@
     end if
 
     end subroutine json_value_remove
+!*****************************************************************************************
+
+!*****************************************************************************************
+!> author: Jacob Williams
+!  date: 4/26/2016
+!
+!  Swap two elements in a JSON structure.
+!  All of the children are carried along as well.
+!
+!@note If both are not associated, then an error is thrown.
+!
+!@note The assumption here is that both variables are part of a valid
+!      [[json_value]] linked list (so the normal `parent`, `previous`,
+!      `next`, etc. pointers are properly associated if necessary).
+!
+!@warning This should not be used to swap an element with one of its
+!         direct children (along the first in the lists), since that would
+!         produce a circular linkage. A check should be added for this...
+!         Only the simple cases where p1/p2 or p2/p1 are parent/child
+!         are currently checked.
+!
+!@warning This is a work-in-progress and has not yet been fully validated.
+
+    subroutine json_value_swap(json,p1,p2)
+
+    implicit none
+
+    class(json_core),intent(inout) :: json
+    type(json_value),pointer       :: p1
+    type(json_value),pointer       :: p2
+
+    logical :: same_parent,first_last
+    type(json_value),pointer :: p1_par_first,p1_par_last,p2_par_first,p2_par_last
+    type(json_value),pointer :: a,b
+    logical :: adjacent
+
+    if (json%exception_thrown) return
+
+    !both have to be associated:
+    if (associated(p1) .and. associated(p2)) then
+
+        !simple check to make sure that they both
+        !aren't pointing to the same thing:
+        if (.not. associated(p1,p2)) then
+
+            !TODO Need to check *all* the `children` pointers, so make sure
+            !     cases like p1%child%...%child => p2 don't occur...
+            !     .... or is that case OK? ....
+            if (associated(p1%parent,p2) .or. associated(p2%parent,p1)) then
+                call json%throw_exception('Error in json_value_swap: '//&
+                                          'cannot swap a parent/child pair')
+            else
+
+                same_parent = ( associated(p1%parent) .and. &
+                                associated(p2%parent) .and. &
+                                associated(p1%parent,p2%parent) )
+                if (same_parent) then
+                    first_last = (associated(p1%parent%children,p1) .and. &
+                                  associated(p2%parent%tail,p2)) .or. &
+                                 (associated(p1%parent%tail,p1) .and. &
+                                  associated(p2%parent%children,p2))
+                else
+                    first_last = .false.
+                end if
+
+                !first, we fix children,tail pointers:
+
+                if (same_parent .and. first_last) then
+
+                    !this is all we have to do for the parent in this case:
+                    call swap_pointers(p1%parent%children,p2%parent%tail)
+
+                elseif (same_parent .and. .not. first_last) then
+
+                    if (associated(p1%parent%children,p1)) then
+                        ! if p1 is the first child of its parent:
+                        p1%parent%children => p2
+                    elseif (associated(p1%parent%children,p2)) then
+                        ! if p2 is the first child of its parent:
+                        p1%parent%children => p1
+                    end if
+                    if (associated(p1%parent%tail,p1)) then
+                        ! if p1 is the last child of its parent:
+                        p1%parent%tail => p2
+                    elseif (associated(p1%parent%tail,p2)) then
+                        p1%parent%tail => p1
+                    end if
+
+                else ! general case: different parents
+
+                    !... see if we can clean this up ...
+
+                    p1_par_first => null()
+                    p1_par_last  => null()
+                    p2_par_first => null()
+                    p2_par_last  => null()
+                    if (associated(p1%parent)) then
+                        if (associated(p1%parent%children,p1)) then
+                            ! if p1 is the first child of its parent:
+                            p1_par_first => p2
+                        else
+                            p1_par_first => p1%parent%children !no change
+                        end if
+                        if (associated(p1%parent%tail,p1)) then
+                            ! if p1 is the last child of its parent:
+                            p1_par_last => p2
+                        else
+                            p1_par_last => p1%parent%tail ! no change
+                        end if
+                    end if
+                    if (associated(p2%parent)) then
+                        if (associated(p2%parent%children,p2)) then
+                            ! if p2 is the first child of its parent:
+                            p2_par_first => p1
+                        else
+                            p2_par_first => p2%parent%children !no change
+                        end if
+                        if (associated(p2%parent%tail,p2)) then
+                            ! if p2 is the last child of its parent:
+                            p2_par_last => p1
+                        else
+                            p2_par_last => p2%parent%tail !no change
+                        end if
+                    end if
+                    if (associated(p1%parent)) then
+                        p1%parent%children  => p1_par_first
+                        p1%parent%tail      => p1_par_last
+                    end if
+                    if (associated(p2%parent)) then
+                        p2%parent%children  => p2_par_first
+                        p2%parent%tail      => p2_par_last
+                    end if
+
+                    call swap_pointers(p1%parent, p2%parent)
+
+                end if
+
+                !now, have to fix previous,next pointers:
+
+                !first, see if they are adjacent:
+                adjacent = associated(p1%next,p2) .or. &
+                           associated(p2%next,p1)
+                if (associated(p2%next,p1)) then    !p2,p1
+                    a => p2
+                    b => p1
+                else    !p1,p2 (or not adjacent)
+                    a => p1
+                    b => p2
+                end if
+                if (associated(a%previous)) a%previous%next => b
+                if (associated(b%next))     b%next%previous => a
+
+                if (adjacent) then
+                    !a comes before b in the original list
+                    b%previous => a%previous
+                    a%next     => b%next
+                    a%previous => b
+                    b%next     => a
+                else
+                    if (associated(a%next))       a%next%previous => b
+                    if (associated(b%previous))   b%previous%next => a
+                    call swap_pointers(a%previous,b%previous)
+                    call swap_pointers(a%next,    b%next)
+                end if
+
+            end if
+
+        else
+            call json%throw_exception('Error in json_value_swap: '//&
+                                      'both pointers must be associated')
+        end if
+
+    end if
+
+    contains
+
+        pure subroutine swap_pointers(s1,s2)
+
+        implicit none
+
+        type(json_value),pointer,intent(inout) :: s1
+        type(json_value),pointer,intent(inout) :: s2
+
+        type(json_value),pointer :: tmp  !! temporary pointer
+
+        if (.not. associated(s1,s2)) then
+            tmp => s1
+            s1  => s2
+            s2  => tmp
+            nullify(tmp)
+        end if
+
+        end subroutine swap_pointers
+
+    end subroutine json_value_swap
 !*****************************************************************************************
 
 !*****************************************************************************************
@@ -2305,7 +2511,7 @@
                     p => p%next
                 else
                     call json%throw_exception('Error in json_value_get_by_index:'//&
-                                         ' p%next is not associated.')
+                                              ' p%next is not associated.')
                     nullify(p)
                     return
                 end if
@@ -2357,6 +2563,11 @@
                 n_children = json%count(me)
                 p => me%children    !start with first one
                 do i=1, n_children
+                    if (.not. associated(p)) then
+                        call json%throw_exception('Error in json_value_get_by_name_chars: '&
+                                                  'Malformed JSON linked list')
+                        return
+                    end if
                     if (allocated(p%name)) then
                         if (p%name == name) return
                     end if
@@ -2565,6 +2776,12 @@
                 element => me%children
                 do i = 1, count
 
+                    if (.not. associated(element)) then
+                        call json%throw_exception('Error in json_value_print: '//&
+                                                  'Malformed JSON linked list')
+                        return
+                    end if
+
                     ! print the name
                     if (allocated(element%name)) then
                         call write_it(repeat(space, spaces)//quotation_mark//&
@@ -2608,6 +2825,12 @@
                 nullify(element)
                 element => me%children
                 do i = 1, count
+
+                    if (.not. associated(element)) then
+                        call json%throw_exception('Error in json_value_print: '//&
+                                                  'Malformed JSON linked list')
+                        return
+                    end if
 
                     ! recursive print of the element
                     call json%json_value_print(element, iunit=iunit, indent=tab,&
---
+++ src/tests/jf_test_16.f90
@@ -0,0 +1,158 @@
+!*****************************************************************************************
+!> author: Jacob Williams
+!  date: 4/26/2016
+!
+! Module for the 16th unit test.
+! Test the `swap` function.
+
+module jf_test_16_mod
+
+    use json_module
+    use, intrinsic :: iso_fortran_env , only: error_unit,output_unit
+
+    implicit none
+
+contains
+
+    subroutine test_16(error_cnt)
+
+    !! Test the `swap` function.
+
+    implicit none
+
+    integer,intent(out) :: error_cnt !! report number of errors to caller
+
+    type(json_core) :: json
+    type(json_value),pointer :: p,p1,p2,q
+
+    write(error_unit,'(A)') ''
+    write(error_unit,'(A)') '================================='
+    write(error_unit,'(A)') '   TEST 16'
+    write(error_unit,'(A)') '================================='
+    write(error_unit,'(A)') ''
+
+    error_cnt = 0
+
+    write(error_unit,'(A)') ''
+    write(error_unit,'(A)') 'Original:'
+    call json%parse(p, '{"cities": ["New York","Los Angeles","Chicago"], '//&
+                       '"value": 1, "flag": true, "struct":{"vec":[1,2,3]}}')
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    !call print_tree(json,p)
+    call json%print(p,error_unit)
+
+    write(error_unit,'(A)') ''
+    write(error_unit,'(A)') 'Swap: cities <-> flag'
+    call json%get(p,'cities',p1)
+    call json%get(p,'flag',p2)
+    call json%swap(p1,p2)
+    !call print_tree(json,p)
+    call json%print(p,output_unit)
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    nullify(p1)
+    nullify(p2)
+
+    write(error_unit,'(A)') ''
+    write(error_unit,'(A)') 'Swap: flag <-> value'
+    call json%get(p,'flag',p1)
+    call json%get(p,'value',p2)
+    call json%swap(p1,p2)
+    !call print_tree(json,p)
+    call json%print(p,output_unit)
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    nullify(p1)
+    nullify(p2)
+
+    write(error_unit,'(A)') ''
+    write(error_unit,'(A)') 'Swap: flag <-> struct.vec'
+    call json%get(p,'flag',p1)
+    call json%get(p,'struct.vec',p2)
+    call json%swap(p1,p2)
+    !call print_tree(json,p)
+    call json%print(p,output_unit)
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    nullify(p1)
+    nullify(p2)
+
+    call json%destroy(p)
+
+    end subroutine test_16
+
+    subroutine print_tree(json,p)
+
+    !! just for debugging. Print some info about the structure.
+
+    implicit none
+
+    type(json_core),intent(inout) :: json
+    type(json_value),pointer,intent(in) :: p
+
+    character(len=:),allocatable :: p_name, name
+    type(json_value),pointer :: q,r
+    integer :: n_children,i
+
+    write(*,*) ''
+    write(*,*) '------------'
+    call json%info(p,name=p_name,n_children=n_children)
+    write(*,*) 'name:       '//p_name
+    write(*,*) 'n_children: ',n_children
+
+    call json%get_parent(p,q)
+    if (associated(q)) then
+        call json%info(q,name=name)
+        write(*,*) 'root parent: '//name
+    end if
+
+    do i=1,n_children
+        call json%get_child(p, i, q)
+        if (associated(q)) then
+            call json%info(q,name=name)
+            write(*,*) 'child ',i,name
+            call json%get_previous(q,r)
+            if (associated(r)) then
+                call json%info(r,name=name)
+                write(*,*) '   prev ',i,name
+            end if
+            call json%get_next(q,r)
+            if (associated(r)) then
+                call json%info(r,name=name)
+                write(*,*) '   next ',i,name
+            end if
+        else
+            call json%print_error_message(error_unit)
+            exit
+        end if
+    end do
+    write(*,*) '------------'
+    write(*,*) ''
+
+    end subroutine print_tree
+
+end module jf_test_16_mod
+!*****************************************************************************************
+
+!*****************************************************************************************
+program jf_test_16
+
+    !! 16th unit test.
+
+    use jf_test_16_mod, only: test_16
+    implicit none
+    integer :: n_errors
+    call test_16(n_errors)
+    if ( n_errors /= 0) stop 1
+
+end program jf_test_16
+!*****************************************************************************************
