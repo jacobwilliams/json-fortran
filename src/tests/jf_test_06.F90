d9c0a5bbb1f6d32950f3607cfcb71baba5456c29
Jacob Williams <jacobwilliams@users.noreply.github.com>
2018-07-22 20:41:55 -0500

    I don't understand why this is now failing on linux. added some debugging prints to see what is going on.

--- src/tests/jf_test_06.F90
+++ src/tests/jf_test_06.F90
@@ -29,8 +29,9 @@ contains
     integer,intent(out) :: error_cnt
 
     type(json_file) :: json
-    integer :: i
+    integer :: i, j
     character(kind=CK,len=:),allocatable :: error_msg
+    character(kind=CK,len=:),allocatable :: expected_error_msg
     logical(LK) :: status_ok
 
     character(len=*),dimension(3),parameter :: files = ['invalid.json ',&
@@ -61,13 +62,20 @@ contains
 
             if (i==1) then
                 call json%check_for_errors(status_ok, error_msg=error_msg)
-                if (error_msg /= &
-                        CK_'Error in parse_array: Unexpected character encountered when parsing array.'//newline//&
-                        CK_'line: 13, character: 1'//newline//&
-                        CK_'}'//newline//&
-                        '^') then
+                expected_error_msg = CK_'Error in parse_array: Unexpected charact9er encountered when parsing array.'//newline//&
+                                     CK_'line: 13, character: 1'//newline//&
+                                     CK_'}'//newline//'^'
+                if (error_msg /= expected_error_msg) then
                     ! verify that the expected error string is present
                     write(error_unit,'(A)') 'Error: unexpected error message string: "'//error_msg//'"'
+                    write(error_unit,'(A)') ''
+                    write(error_unit,*) 'len(error_msg)          = ', len(error_msg)
+                    write(error_unit,*) 'len(expected_error_msg) = ', len(expected_error_msg)
+                    do j = 1, min(len(error_msg), len(expected_error_msg))
+                        if (error_msg(j:j) /= expected_error_msg(j:j)) then
+                            write(error_unit,'(I3,1X,A,A,A,A,A)') j, '"', error_msg(j:j), '" /= "', expected_error_msg(j:j), '"'
+                        end if
+                    end do
                     error_cnt = error_cnt + 1
                 end if
             end if
